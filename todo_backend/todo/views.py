88179df792621722c27548c7e75423c6a26e5d7a
rjLelis <renatojlelis@gmail.com>
2019-05-02 22:36:11 -0300

    Create todo serializer & todo get/post api

--- todo_backend/todo/views.py
+++ todo_backend/todo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import TodoSerializer
+from .models import Todo
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def create_and_list_todo(request):
+    if request.method == 'POST':
+        todo = Todo(title=request.data['title'], description=request.data['description'])
+        todo.save()
+        todo_serializer = TodoSerializer(todo)
+        return Response(todo_serializer.data)
+    todos = Todo.objects.all()
+    todos_serializer = TodoSerializer(todos, many=True)
+    return Response(todos_serializer.data)
