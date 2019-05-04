fc9de3fd6782edc35dba954cf2ca786b8e31d7f2
rjLelis <renatojlelis@gmail.com>
2019-05-03 22:15:48 -0300

    POC of the django/react integration

--- todo_frontend/src/App.js
+++ todo_frontend/src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state ={
+    title: '',
+    description: ''
+  }
+
+  componentDidMount() {
+    
+    fetch(`http://localhost:8080/api/todo/1`,
+    {
+      method: 'GET',
+      headers: new Headers({'Content-type': 'application/json'})
+    }).then(response => {
+      if(response.status !== 200){
+        return response.message
+      }
+      return response.json()
+    }).then(data => this.setState({title: data.title, description: data.description})
+    )
+  }
+
+
+  render() {
+    const {title, description} = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {title}
+          </p>
+          <p>
+            {description}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
