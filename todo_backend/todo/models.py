04ff4b4882d8aac0b5e6a9636bcb1e3fede256c4
rjLelis <renatojlelis@gmail.com>
2019-05-13 13:34:14 -0300

    Add __str__ method to Todo model

--- todo_backend/todo/models.py
+++ todo_backend/todo/models.py
@@ -5,3 +5,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=100, null=False)
     description = models.TextField()
     done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
