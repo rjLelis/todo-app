1198852fdfc60f30ee0a2029c413f8ead4c923b4
rjLelis <renatojlelis@gmail.com>
2019-05-20 17:24:31 -0300

    Finish controller for todo module & integration with view, test required

--- todo_backend/todo/controller.py
+++ todo_backend/todo/controller.py
@@ -1,15 +1,112 @@
-from todo.models import Todo
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework import status
 
-def get_all_todo():
-    return Todo.objects.all()
+
+def get_all_todos():
+    todos = Todo.objects.all()
+    todos_serialized = TodoSerializer(todos, many=True)
+    return {'todos': todos_serialized.data, 'status': status.HTTP_200_OK}
 
 
 def get_todo_by_id(todo_id):
-    todo = Todos.objects.get(id=todo_id)
-    if todo is None:
-        raise Todo.DoesNotExist
-    else:
-        return todo
+    try:
+        todo = Todo.objects.get(id=todo_id)
+        todo_serialized = TodoSerializer(todo)
+        return True, {'todo':todo_serialized.data, 'status': status.HTTP_200_OK}
 
-def create_or_update_todo(todo):
-    
+    except TypeError:
+        error = {'error_message':'The todo id must be provided', 'status': status.HTTP_400_BAD_REQUEST}
+        return False, error
+
+    except Todo.DoesNotExist:
+        error = {'error_message':f'Todo not found with id {todo_id}', 'status': status.HTTP_404_NOT_FOUND}
+        return False, error
+
+
+def create_todo(todo):
+    try:
+        title = todo['title']
+
+    except KeyError:
+        error = {'error_message':'The todo title must be provided', 'status': status.HTTP_400_BAD_REQUEST}
+        return False, error
+        raise ValueError()
+
+    finally:
+        try:
+            description = todo['description']
+
+        except KeyError:
+            description = ''
+        
+        finally:
+            done = False
+            new_todo = Todo(title=title, description=description, done=done)
+            new_todo.save()
+            new_todo_serialized = TodoSerializer(new_todo)
+            return True, {'todo': new_todo_serialized.data, 'status': status.HTTP_201_CREATED}
+
+
+def update_todo(todo_id, todo_update):
+    try:
+        todo = Todo.objects.get(id=todo_id)
+        todo.title = todo_update['title']
+
+    except KeyError:
+        error = {'error_message':'The todo title must be provided', 'status': status.HTTP_400_BAD_REQUEST}
+        return False, error
+
+    except TypeError:
+        error = {'error_message':'The todo id must be provided', 'status': status.HTTP_400_BAD_REQUEST}
+        return False, error
+
+    except Todo.DoesNotExist:
+        error = {'error_message': f'Todo not found with id {todo_id}', 'status': status.HTTP_404_NOT_FOUND}
+        return False, error
+
+    finally:
+        try:
+            todo.description = todo_update['description']
+            todo.done = todo_update['done']
+
+        except KeyError:
+            pass
+
+        finally:
+            todo.save()
+            todo_serialized = TodoSerializer(todo)
+            return True, {'todo': todo_serialized.data, 'status': status.HTTP_200_OK}
+
+
+def delete_todo_by_id(todo_id):
+    try:
+        todo = Todo.objects.get(id=todo_id)
+    except TypeError:
+        error = {'error_message': 'The todo id must be provided', 'status': status.HTTP_400_BAD_REQUEST}
+        return False, error
+    except Todo.DoesNotExist:
+        error = {'error_message': f'Todo not found with id {todo_id}', 'status': status.HTTP_404_NOT_FOUND}
+        return False, error
+    finally:
+        title = todo.title
+        todo.delete()
+        return True, {'message': f'The todo "{title}" has been deleted', 'status':status.HTTP_204_NO_CONTENT}
+
+
+def finish_todo(todo_id):
+    try:
+        todo = Todo.objects.get(id=todo_id)
+        if todo.done:
+            error = {'error_message': 'The todo "{todo.title}" has already been finished', 'status': status.HTTP_304_NOT_MODIFIED}
+            return False, error
+        else:
+            todo.done = True
+            todo.save()
+            return True, {'message':'The todo "{todo.title}" has been finished', 'status': status.HTTP_200_OK}
+    except TypeError:
+        error = {'error_message': 'The todo id must be provided', 'status': status.HTTP_400_BAD_REQUEST}
+        return False, error
+    except Todo.TodoDoesNotExist:
+        error = {'error_message': f'Todo not found with id {todo_id}', 'status': status.HTTP_404_NOT_FOUND}
+        return False, error
--- todo_backend/todo/views.py
+++ todo_backend/todo/views.py
@@ -1,57 +1,59 @@
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import TodoSerializer
+from rest_framework.response import Response
+
+from . import controller
 from .models import Todo
+from .serializers import TodoSerializer
 
 
 @api_view(['GET', 'POST'])
 def create_and_list_todo(request):
+
     if request.method == 'POST':
-        title = request.data['title']
-        description = request.data['description']
-        done = request.data['done']
-        todo = Todo(title=title, description=description, done=done)
-        todo.save()
-        todo_serializer = TodoSerializer(todo)
-        return Response(todo_serializer.data, status=status.HTTP_201_CREATED)
-    todos = Todo.objects.all()
-    todos_serializer = TodoSerializer(todos, many=True)
-    return Response(todos_serializer.data, status=status.HTTP_200_OK)
+            is_success, response = controller.create_todo(request.data)
+            if is_success:
+                return Response(response['todo'], status=response['status'])
+            else:
+                return Response(response['error_message'], status=response['status'])
+    
+    else:
+        response = controller.get_all_todos()
+        return Response(response['todos'], status=response['status'])
 
 
 @api_view(['GET', 'DELETE', 'PUT'])
 def get_delete_update_todo(request, todo_id):
-    try:
-        todo = Todo.objects.get(id=todo_id)
-    except Todo.DoesNotExist:
-        return Response({'message': f'Todo not found with id {todo_id}'}, status=status.HTTP_404_NOT_FOUND)
-    if request.method == 'GET':
-        todo_serializer = TodoSerializer(todo)
-        return Response(todo_serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'DELETE':
-        message = f'The todo "{todo.title}" has been deleted'
-        todo.delete()
-        return Response(data={'message': message}, status=status.HTTP_204_NO_CONTENT)
+
+    is_success, response = controller.get_todo_by_id(todo_id)
+    if is_success:
+        if request.method == 'GET':
+            return Response(response['todo'], status=response['status'])
+
+        elif request.method == 'PUT':           
+            is_success, response = controller.update_todo(todo_id, request.data)
+            if is_success:
+                return Response(response['todo'], status=response['status'])
+                
+            else:
+                return Response(response['error_message'], status=response['status'])
+
+        elif request.method == 'DELETE':
+            is_success, response = controller.delete_todo_by_id(todo_id)
+            if is_success:
+                return Response(response['message'], status=response['status'])
+
+            else:
+                return Response(response['error_message'], status=response['status'])
+
     else:
-        try:
-            todo.title = request.data['title']
-            todo.description = request.data['description']
-            todo.done = request.data['done']
-            todo.save()
-            todo_serializer = TodoSerializer(todo)
-            return Response(todo_serializer.data, status=status.HTTP_200_OK)
-        except KeyError:
-            return Response({'message': 'All parameters of the todo must be sent in order to update the todo'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(response['error_message'], status=response['status'])
 
 
 @api_view(['PUT'])
 def finish_todo(request, todo_id):
-    try:
-        todo = Todo.objects.get(id=todo_id)
-        todo.done = True
-        todo.save()
-        todo_serializer = TodoSerializer(todo)
-        return Response(data={'todo': todo_serializer.data, 'message': f'The todo "{todo.title}" has been finished'}, status=status.HTTP_200_OK)
-    except Todo.DoesNotExist:
-        return Response({'message': f'Todo not found with id {todo_id}'}, status=status.HTTP_404_NOT_FOUND)
+    is_success, response = controller.finish_todo(todo_id)
+    if is_success:
+        return Response(response['message'], status=response['status'])
+    else:
+        return Response(response['error_message'], status=response['status'])
