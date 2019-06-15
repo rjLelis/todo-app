aad4f9c8c543c8e8f2880e9ebf017a4e3e760484
rjLelis <renatojlelis@gmail.com>
2019-06-15 19:22:39 -0300

    Refactor on get_delete_update_todo function

--- todo_backend/todo/views.py
+++ todo_backend/todo/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from . import todo_controller as todo_controller
+from . import controller as todo_controller
 
 
 @api_view(['GET', 'POST'])
@@ -20,19 +20,15 @@ def create_and_list_todo(request):
 def get_delete_update_todo(request, todo_id):
 
     response = todo_controller.get_todo_by_id(todo_id)
-    if response['status'] == 200:
-        if request.method == 'GET':
-            return Response(data=response['data'], status=response['status'])
+    if request.method == 'GET':
+        return Response(data=response['data'], status=response['status'])
 
-        elif request.method == 'PUT':           
-            response = todo_controller.update_todo(todo_id, request.data)
-            return Response(data=response['data'], status=response['status'])
-                
-        elif request.method == 'DELETE':
-            response = todo_controller.delete_todo_by_id(todo_id)
-            return Response(data=response['data'], status=response['status'])
-
-    else:
+    elif request.method == 'PUT':           
+        response = todo_controller.update_todo(todo_id, request.data)
+        return Response(data=response['data'], status=response['status'])
+            
+    elif request.method == 'DELETE':
+        response = todo_controller.delete_todo_by_id(todo_id)
         return Response(data=response['data'], status=response['status'])
 
 
